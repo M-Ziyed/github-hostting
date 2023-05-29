f56f780a4cbadd4f3eac01b2302ae672dbbb8310
M-Ziyed <134716475+M-Ziyed@users.noreply.github.com>
2023-05-30 01:55:46 +0300

    secend commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { ProductsComponent } from './products/products.component';
+import { FristAppComponent } from './frist-app/frist-app.component';
+import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { AlertModule  } from  'ngx-bootstrap/alert' ;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ShoppingCartComponent,
+    NavBarComponent,
+    MyOrdersComponent,
+    AdminOrdersComponent,
+    AdminOrdersComponent,
+    ProductsComponent,
+    FristAppComponent,
+  
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BsDropdownModule.forRoot(),
+    AlertModule.forRoot(),
+    // AccordionModule,
+    NgbModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
